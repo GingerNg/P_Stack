2f6b49444b77f482cce55a1efb19e736be8d7741
ginger <jinjie.wu@fir.ai>
2019-01-03 10:39:33 +0800

    @update

--- tools/batch_rename.py
+++ tools/batch_rename.py
@@ -17,7 +17,7 @@ t = BatchRename(fmt)  # create a object
 print(t)
 date = time.strftime('%d%d%y')  # return a string as time
 print(date)
-for i, filename in enumerate(photofiles):  # i : 0,1,2...
+for i, filename in enumerate(photofiles):  # i : 0,1,2...   (index value)
     base, ext = os.path.splitext(filename)  # wordsegmentation
     print(base, ext)
     newname = t.substitute(d=date, n=i, f=ext)
