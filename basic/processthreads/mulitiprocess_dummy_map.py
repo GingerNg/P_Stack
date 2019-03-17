dd1d57c4fffc49469b6788e30d8637c80562e1f2
ginger <jinjie.wu@fir.ai>
2019-03-17 13:30:19 +0800

    @update 多线程/多进程

---
+++ basic/processthreads/mulitiprocess_dummy_map.py
@@ -0,0 +1,45 @@
+"""
+dummy 是 multiprocessing 模块的完整克隆，唯一的不同在于 multiprocessing 作用于进程，
+而 dummy 模块作用于线程（因此也包括了 Python 所有常见的多线程限制）
+CPU密集型--多进程
+IO密集型--多线程
+"""
+import requests
+# import urllib3
+from multiprocessing.dummy import Pool as ThreadPool
+
+# 一般来说，执行 CPU 密集型任务时，调用越多的核速度就越快。
+# 但是当处理网络密集型任务时，事情有有些难以预计了，通过实验来确定线程池的大小才是明智的。
+pool = ThreadPool(4)
+
+urls = [
+    'http://www.python.org',
+    'http://www.python.org/about/',
+    'http://www.onlamp.com/pub/a/python/2003/04/17/metaclasses.html',
+    'http://www.python.org/doc/',
+    'http://www.python.org/download/',
+    'http://www.python.org/getit/',
+    'http://www.python.org/community/',
+    'https://wiki.python.org/moin/',
+    'http://planet.python.org/',
+    'https://wiki.python.org/moin/LocalUserGroups',
+    'http://www.python.org/psf/',
+    'http://docs.python.org/devguide/',
+    'http://www.python.org/community/awards/'
+    # etc..
+    ]
+
+def task(url):
+    res = requests.get(url=url)
+    print(res.content)
+
+
+# Make the Pool of workers
+# Open the urls in their own threads
+# and return the results
+results = pool.map(task, urls)
+#close the pool and wait for the work to finish
+pool.close()
+pool.join()
+
+
--- basic/processthreads/using_processes.py
+++ basic/processthreads/using_processes.py
@@ -20,15 +20,15 @@ import os
 
 # 子进程要执行的代码
 def run_proc(name):
-    print 'Run child process %s (%s)...' % (name, os.getpid())
+    print ('Run child process %s (%s)...' % (name, os.getpid()))
 
 if __name__=='__main__':
-    print 'Parent process %s.' % os.getpid()
+    print ('Parent process %s.' % os.getpid())
     # 创建子进程时，只需要传入一个执行函数和函数的参数，创建一个Process实例，
     # 用start()方法启动，这样创建进程比fork()还要简单。
     p = Process(target=run_proc, args=('test',))
-    print 'Process will start.'
+    print ('Process will start.')
     p.start()
     p.join() # join()方法可以等待子进程结束后再继续往下运行，通常用于进程间的同步。
-    print 'Process end.'
+    print ('Process end.')
 
