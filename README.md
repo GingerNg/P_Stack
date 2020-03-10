45f6ea5ed0d61b344751b39a428d296dadb666b2
jinjie.wu <jinjie.wu@fir.ai>
2020-03-10 20:03:20 +0800

    @update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 # Python Stack
 
  codes, links and notes
@@ -6,3 +12,6 @@
 ascii  --> GB2312 --> GBK  --> GB18030
 ISO --> unicode编码以u开头   --> UTF8
 GBK和UTF8是一个层面的东西
+
+
+
--- utils/email_utils.py
+++ utils/email_utils.py
@@ -10,71 +10,69 @@ import os
 import sys
 
 
-class Email (object ):
-    def __init__ (self,domain,port,account,password,display=None):
-        """ 
-        Gmail("username@gmail.com","xxxx") 
-        """ 
+class Email (object):
+    def __init__(self, domain, port, account, password, display=None):
+        """
+        Gmail("username@gmail.com","xxxx")
+        """
         self.domain = domain
         self.port = port
         self.account = account
         self.password = password
         self.display = display or self.account
 
-    def send (self,to,title,content,_t=str(datetime.datetime.now())):
-        """ 
-        send('username@gmail.com,other@domain.com") 
-        """ 
+    def send(self, to, title, content, _t=str(datetime.datetime.now())):
+        """
+        send('username@gmail.com,other@domain.com")
+        """
         try:
             server = smtplib.SMTP(self.domain, self.port)
         except smtplib.SMTPConnectError:
             return 0, u"无法连接邮件服务器".encode("utf-8")
-        #server.set_debuglevel(1)
-        server.docmd("EHLO server" )
+        # server.set_debuglevel(1)
+        server.docmd("EHLO server")
         server.starttls()
         try:
-            server.login(self.account,self.password)
+            server.login(self.account, self.password)
         except smtplib.SMTPAuthenticationError:
             return 0, u"用户名或者密码错误".encode("utf-8")
-        #邮件主体
+        # 邮件主体
         h = Header(title, 'utf-8')
-        
-        
-        #创建一个带附件的实例
+
+        # 创建一个带附件的实例
         #msg = MIMEMultipart()
-        msg = MIMEText(content,'html','utf-8')
-        
-        #构造附件
+        msg = MIMEText(content, 'html', 'utf-8')
+
+        # 构造附件
         #att = MIMEText(open(fn, 'rb').read(), 'base64', 'utf-8')
         #att["Content-Type"] = 'application/octet-stream'
         #att["Content-Disposition"] = 'attachment; filename="%s"'%os.path.basename(fn)
-        #msg.attach(att)
-        
-        msg['Subject' ] = h
-        msg['From' ] = self.display
-        msg['To' ] = to
-        msg['Date' ] = time.strftime('%a, %d %b %Y %H:%M:%S %z') 
-        
+        # msg.attach(att)
+
+        msg['Subject'] = h
+        msg['From'] = self.display
+        msg['To'] = to
+        msg['Date'] = time.strftime('%a, %d %b %Y %H:%M:%S %z')
+
         try:
-            server.sendmail(self.account, to.split(',') ,msg.as_string())
+            server.sendmail(self.account, to.split(','), msg.as_string())
         except smtplib.SMTPRecipientsRefused:
             return 0, u"邮件发送失败".encode("utf-8")
         server.close()
         return 1, u"发送成功"
-        
-        
-        
+
+
 EMAIN_DOMAIN = 'smtp.partner.outlook.cn'
 SERVER_PORT = 587
 EMAIL_USER_NAME = "XXX@XXX.com"
 EMAIL_PASSWORD = "********"
 
-def send(to_list,title, content):
-    mail = Email(EMAIN_DOMAIN,SERVER_PORT,EMAIL_USER_NAME,EMAIL_PASSWORD,
-                               'XXXX@XXXX.com')
+
+def send(to_list, title, content):
+    mail = Email(EMAIN_DOMAIN, SERVER_PORT, EMAIL_USER_NAME, EMAIL_PASSWORD,
+                 'XXXX@XXXX.com')
     return mail.send(",".join(to_list), title, content)
-    
-    
+
 
 if __name__ == "__main__":
-    print (send(["XXX@XXX.com","xxx@XXX.com"], "XXXX，XXXX", "XXXX"))
+    print(send(["XXX@XXX.com", "xxx@XXX.com"], "XXXX，XXXX", "XXXX"))
